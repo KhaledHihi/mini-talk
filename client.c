a85f29e470576e986333af628495f3c91bbe5393
Khaled Hihi <khhihi@e1r13p2.1337.ma>
2025-03-04 23:07:59 +0000

    trying to send bites

--- client.c
+++ client.c
@@ -6,12 +6,39 @@
 /*   By: khhihi <khhihi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 19:44:35 by khhihi            #+#    #+#             */
-/*   Updated: 2025/03/03 20:19:29 by khhihi           ###   ########.fr       */
+/*   Updated: 2025/03/04 23:04:36 by khhihi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mini.h"
 
+
+
+void	send_bits(int pid, char c)
+{
+	int bit;
+
+	bit = 7;
+	while (bit >= 0)
+	{
+		if (c & (1 << bit))
+			kill (pid, SIGUSR1);
+		else
+			kill (pid, SIGUSR2);
+		bit--;
+		usleep(50);
+	}
+}
+
+void	send_message(int pid, char *msg)
+{
+	static int	i;
+
+	i = 0;
+	while (msg[i])
+		send_bits(pid, msg[i++]);
+	kill(pid, SIGUSR2);
+}
 int	main(int ac, char *av[])
 {
 	int	pid;
@@ -23,4 +50,5 @@ int	main(int ac, char *av[])
 		return (ft_putendl_fd("invalid pid ", 1), 1);
 	if (ft_strlen(av[2]) == 0)
 		return (ft_putendl_fd("Write a message", 1), 1);
+	send_message(pid, av[2]);
 }
--- server.c
+++ server.c
@@ -6,17 +6,39 @@
 /*   By: khhihi <khhihi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 19:36:22 by khhihi            #+#    #+#             */
-/*   Updated: 2025/03/03 20:29:37 by khhihi           ###   ########.fr       */
+/*   Updated: 2025/03/04 22:56:09 by khhihi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mini.h"
+#include <unistd.h>
 
-int main()
+void ft_handler(int signum)
 {
+    static char byte;
+    static int i;
+
+    if (signum == SIGUSR1)
+        byte |= (1 << (7 - i));
+    i++;
+    if (i == 8)
+    {
+        ft_putchar_fd(byte, 1);
+        i = 0;
+        byte = 0;
+		write(1,"\n",1);
+    }
+	usleep(50);
+}
+int main(int ac, char **av)
+{
+	if (ac != 1)
+		return (ft_putstr_fd("Error: wrong format.\nTry: ./server", 1), 1);
 	ft_putstr_fd ("The pid of this server is : ", 1);
 	ft_putnbr_fd (getpid(), 1);
-	write(1, "\n", 1);
+	signal(SIGUSR1, ft_handler);
+	signal(SIGUSR2, ft_handler);
 	while (1)
 		usleep(50);
+
 }
