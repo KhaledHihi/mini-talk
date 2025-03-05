765357b2ac3eeb5a2c9d6c2937c36da647ccda42
Khaled Hihi <khhihi@e1r3p4.1337.ma>
2025-03-05 17:26:36 +0000

    fix the code

--- client.c
+++ client.c
@@ -6,39 +6,61 @@
 /*   By: khhihi <khhihi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 19:44:35 by khhihi            #+#    #+#             */
-/*   Updated: 2025/03/04 23:04:36 by khhihi           ###   ########.fr       */
+/*   Updated: 2025/03/05 17:24:54 by khhihi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mini.h"
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
 
+int g_confirmation = 0;
 
+void	ft_handler(int signum)
+{
+	if (signum == SIGUSR1)
+		g_confirmation = 1;
+	else if (signum == SIGUSR2)
+	{
+		ft_putendl_fd("The message was received successfully.", 1);
+		exit(0);
+	}
+}
 
 void	send_bits(int pid, char c)
 {
-	int bit;
+	int	bit;
 
 	bit = 7;
+	if (kill(pid, 0) < 0)
+	{
+		ft_putendl_fd("Invalid PID", 1);
+		exit(EXIT_FAILURE);
+	}
 	while (bit >= 0)
 	{
+		g_confirmation = 0;
 		if (c & (1 << bit))
-			kill (pid, SIGUSR1);
+			kill(pid, SIGUSR1);
 		else
-			kill (pid, SIGUSR2);
+			kill(pid, SIGUSR2);
 		bit--;
-		usleep(50);
+		while (!g_confirmation)
+			usleep(100);
 	}
 }
 
 void	send_message(int pid, char *msg)
 {
-	static int	i;
+	int	i;
 
 	i = 0;
 	while (msg[i])
 		send_bits(pid, msg[i++]);
-	kill(pid, SIGUSR2);
+	send_bits(pid, '\0'); // Send null-terminator to signal end of message
 }
+
 int	main(int ac, char *av[])
 {
 	int	pid;
@@ -46,9 +68,17 @@ int	main(int ac, char *av[])
 	if (ac != 3)
 		return (ft_putendl_fd("Error: wrong format.\nTry: ./client <PID> <MESSAGE>", 1), 1);
 	pid = ft_atoi(av[1]);
-	if (pid < 0)
-		return (ft_putendl_fd("invalid pid ", 1), 1);
+	if (pid <= 0)
+		return (ft_putendl_fd("Invalid PID.", 1), 1);
 	if (ft_strlen(av[2]) == 0)
-		return (ft_putendl_fd("Write a message", 1), 1);
+		return (ft_putendl_fd("Error: Write a message", 1), 1);
+
+	// Setup signal handlers before sending the message
+	signal(SIGUSR1, ft_handler);
+	signal(SIGUSR2, ft_handler);
+
 	send_message(pid, av[2]);
+
+	while (1) // Keep the client alive until confirmation is received
+		usleep(100);
 }
--- server.c
+++ server.c
@@ -5,40 +5,61 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: khhihi <khhihi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/03 19:36:22 by khhihi            #+#    #+#             */
-/*   Updated: 2025/03/04 22:56:09 by khhihi           ###   ########.fr       */
+/*   Created: 2025/03/03 19:44:35 by khhihi            #+#    #+#             */
+/*   Updated: 2025/03/05 17:24:44 by khhihi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mini.h"
+#include <signal.h>
 #include <unistd.h>
 
-void ft_handler(int signum)
+void	ft_handler(int signum, siginfo_t *info, void *context)
 {
-    static char byte;
-    static int i;
+	static int	byte = 0;
+	static int	bit = 0;
 
-    if (signum == SIGUSR1)
-        byte |= (1 << (7 - i));
-    i++;
-    if (i == 8)
-    {
-        ft_putchar_fd(byte, 1);
-        i = 0;
-        byte = 0;
-		write(1,"\n",1);
-    }
-	usleep(50);
+	(void)context;
+	if (signum == SIGUSR1)
+		byte |= (1 << (7 - bit));
+	bit++;
+
+	if (bit == 8)
+	{
+		if (byte == '\0')
+		{
+			ft_putchar_fd('\n', 1);
+			kill(info->si_pid, SIGUSR2);
+		}
+		else
+			ft_putchar_fd(byte, 1);
+		bit = 0;
+		byte = 0;
+	}
+	kill(info->si_pid, SIGUSR1);
 }
-int main(int ac, char **av)
+
+void	sig_setup(int sig, void (*handler1)(int, siginfo_t *, void *))
 {
-	if (ac != 1)
-		return (ft_putstr_fd("Error: wrong format.\nTry: ./server", 1), 1);
-	ft_putstr_fd ("The pid of this server is : ", 1);
-	ft_putnbr_fd (getpid(), 1);
-	signal(SIGUSR1, ft_handler);
-	signal(SIGUSR2, ft_handler);
+	struct sigaction	sigo;
+
+	sigo.sa_sigaction = handler1;
+	sigo.sa_flags = SA_SIGINFO;
+	sigemptyset(&sigo.sa_mask);
+	sigaction(sig, &sigo, NULL);
+}
+
+int	main(int ac, char **av)
+{
+    if (ac != 1)
+        return (ft_putstr_fd("Invalid arguments\nTry: ./server", 1), 1);
+	ft_putstr_fd("The PID of this server is: ", 1);
+	ft_putnbr_fd(getpid(), 1);
+	ft_putchar_fd('\n', 1);
+
+	sig_setup(SIGUSR1, ft_handler);
+	sig_setup(SIGUSR2, ft_handler);
+
 	while (1)
 		usleep(50);
-
 }
